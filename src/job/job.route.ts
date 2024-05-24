7dd1a9f9311c9f674b9d00dedfea7b49904d2023
Trần Trương Văn <vanttse170128@fpt.edu.vn>
2024-05-24 15:45:44 +0700

    fix import path middleware

--- src/job/job.route.ts
+++ src/job/job.route.ts
@@ -3,7 +3,7 @@ import 'dotenv/config' // To read CLERK_SECRET_KEY and CLERK_PUBLISHABLE_KEY
 import express from 'express'
 import { container } from '../inversify.config'
 import { JobController } from './job.controller'
-import { validateRequestData } from 'src/middleware/validate-request-data.middleware'
+import { validateRequestData } from '../middleware/validate-request-data.middleware'
 import { getJobsSchema } from './job.validation'
 
 const router = express.Router()
